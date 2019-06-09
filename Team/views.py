2baae674b2348fd9e674c8026e21265d0318480e
iam-ruk <rukishore@hotmail.com>
2019-06-09 17:49:54 +0530

    add the UserPassesTestMixin

--- Team/views.py
+++ Team/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Player
 from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from django.urls import reverse_lazy
 # Create your views here.
 
@@ -31,16 +31,32 @@ class PlayerCreateView(LoginRequiredMixin,CreateView):
         return super().form_valid(form)
 
 
-class PlayerUpdateView(LoginRequiredMixin,UpdateView):
+
+class PlayerUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
     model = Player
     fields = ['name','skill','team','dept']
     template_name = 'Team/update.html'
 
+    def form_valid(self, form):
+        form.instance.user_name = self.request.user
+        return super().form_valid(form)
 
-class PlayerDeleteView(DeleteView):
+    def test_func(self):
+        player=self.get_object()
+        if player.user_name == self.request.user:
+            return True
+        return False
+
+class PlayerDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
     model = Player
     template_name = 'Team/delete.html'
     success_url = reverse_lazy('team-home')
+
+    def test_func(self):
+        player = self.get_object()
+        if player.user_name == self.request.user:
+            return True
+        return False
 
 def contact(request):
     return render(request,'Team/contacts.html')
