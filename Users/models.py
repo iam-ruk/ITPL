2aebcde41ee3124fb8273e636a16ba5a910ad785
iam-ruk <rukishore@hotmail.com>
2019-06-09 13:46:09 +0530

    add profile pic and signals

--- Users/models.py
+++ Users/models.py
@@ -12,14 +12,14 @@ class Profile(models.Model):
         return str(self.user.username)
 
     def save(self,*args,**kwargs):
-        super().save()
-        img=Image.open(self.image.path)
+        if self.image.path:
+            img=Image.open(self.image.path)
 
-        if img.height >300 or img.width >300:
-            output_size=(300,300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+            if img.height >300 or img.width >300:
+                output_size=(300,300)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
 
-
+        return super().save(*args, **kwargs)
 
 
