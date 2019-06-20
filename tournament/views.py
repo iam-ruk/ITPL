441df1adf262cbdf6f72c5f5cdd7772bfb1bc31b
iam-ruk <rukishore@hotmail.com>
2019-06-20 21:27:02 +0530

    add views and base.html

--- tournament/views.py
+++ tournament/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 from django.views.generic import ListView,DetailView
 # Create your views here.
 from .models import Match,MatchDescription
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class MatchListView(ListView):
+class MatchListView(LoginRequiredMixin,ListView):
     model = Match
     template_name = 'tournament/schedule.html'
     context_object_name = 'matches'
 
 
 
-class MatchDetailView(DetailView):
+class MatchDetailView(LoginRequiredMixin,DetailView):
     model = Match
     template_name ='tournament/detail.html'
     context_object_name = 'match'
